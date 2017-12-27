a79171c8b9152ecf1660fbd34442011da365977a
Park Ju Hyung <qkrwngud825@gmail.com>
2017-12-27 18:29:42 +0900

    Add initial build tools
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

---
+++ build_kernel.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+export KERNELDIR=`readlink -f .`
+export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
+export PARTITION_SIZE=67108864
+
+echo "kerneldir = $KERNELDIR"
+echo "ramfs_source = $RAMFS_SOURCE"
+
+RAMFS_TMP="/tmp/arter97-op5-ramdisk"
+
+echo "ramfs_tmp = $RAMFS_TMP"
+cd $KERNELDIR
+
+if [ "${1}" = "skip" ] ; then
+	echo "Skipping Compilation"
+else
+	echo "Compiling kernel"
+	cp defconfig .config
+	make "$@" || exit 1
+fi
+
+echo "Building new ramdisk"
+#remove previous ramfs files
+rm -rf '$RAMFS_TMP'*
+rm -rf $RAMFS_TMP
+rm -rf $RAMFS_TMP.cpio
+#copy ramfs files to tmp directory
+cp -ax $RAMFS_SOURCE $RAMFS_TMP
+cd $RAMFS_TMP
+
+find . -name '*.sh' -exec chmod 755 {} \;
+
+$KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
+
+#clear git repositories in ramfs
+find . -name .git -exec rm -rf {} \;
+find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
+cd $KERNELDIR
+rm -rf $RAMFS_TMP/tmp/*
+
+cd $RAMFS_TMP
+find . | fakeroot cpio -H newc -o | gzip -9 > $RAMFS_TMP.cpio.gz
+ls -lh $RAMFS_TMP.cpio.gz
+cd $KERNELDIR
+
+echo "Making new boot image"
+gcc -w -s -pipe -O2 -Itools/libmincrypt -o tools/mkbootimg/mkbootimg tools/libmincrypt/*.c tools/mkbootimg/mkbootimg.c
+tools/mkbootimg/mkbootimg --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb --ramdisk $RAMFS_TMP.cpio.gz --cmdline 'androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 buildvariant=user' --base 0x00008000 --pagesize 4096 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o $KERNELDIR/boot.img
+GENERATED_SIZE=$(stat -c %s boot.img)
+if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
+	echo "boot.img size larger than partition size!" 1>&2
+	exit 1
+fi
+
+echo "done"
+ls -al boot.img
+echo ""
---
+++ kernelzip/system/addon.d/97-arter97.sh
@@ -0,0 +1,37 @@
+#!/sbin/sh
+
+. /tmp/backuptool.functions
+
+list_files() {
+cat <<EOF
+etc/wifi/WCNSS_qcom_cfg.ini
+EOF
+}
+
+case "$1" in
+  backup)
+    mv /dev/block/bootdevice/by-name/boot /dev/block/bootdevice/by-name/boot.bak
+    list_files | while read FILE DUMMY; do
+      backup_file $S/"$FILE"
+    done
+  ;;
+  restore)
+    list_files | while read FILE REPLACEMENT; do
+      R=""
+      [ -n "$REPLACEMENT" ] && R="$S/$REPLACEMENT"
+      [ -f "$C/$S/$FILE" ] && restore_file $S/"$FILE" "$R"
+    done
+  ;;
+  pre-backup)
+    # Stub
+  ;;
+  post-backup)
+    # Stub
+  ;;
+  pre-restore)
+    # Stub
+  ;;
+  post-restore)
+    # Stub
+  ;;
+esac
