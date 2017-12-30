arter97/android_kernel_oneplus_msm8996-old

Makefile: disable store merging for GCC 7
Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

Makefile
@@ -249,7 +249,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/x86/ -e s/x86_64/x86/ \
 # Default value for CROSS_COMPILE is not to prefix executables
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 override ARCH		:= arm64
-override CROSS_COMPILE	:= /home/arter97/linaro-64/bin/aarch64-linux-android-
+override CROSS_COMPILE	:= /home/arter97/linaro-7.1/bin/aarch64-linux-gnu-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
@@ -405,7 +405,7 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -std=gnu89
 
 # Optimization for Kryo
-KBUILD_CFLAGS	+= -mcpu=cortex-a57+crc+crypto
+KBUILD_CFLAGS	+= -mcpu=cortex-a57+crc+crypto -fno-store-merging
 
 # Kryo doesn't need 835769/843419 erratum fixes.
 # Some toolchains enable those fixes automatically, so opt-out.

arter97/android_kernel_oneplus_msm8998-1

Makefile
@@ -398,7 +398,7 @@ KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -std=gnu89 $(call cc-option,-fno-PIE)
 
 # Optimization for msm8998
-KBUILD_CFLAGS	+= -mcpu=cortex-a73+crc+crypto
+KBUILD_CFLAGS	+= -mcpu=cortex-a73+crc+crypto -fno-store-merging
 
 # Kryo doesn't need 835769/843419 erratum fixes.
 # Some toolchains enable those fixes automatically, so opt-out.