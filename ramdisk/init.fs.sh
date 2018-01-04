9ffcfdd2e93c0c55f268f3b6906faf1c62942d71
Park Ju Hyung <qkrwngud825@gmail.com>
2018-01-04 22:55:37 +0900

    ramdisk: fix support for disabled encryption
    Check original fstab and see if fileencryption=ice is there
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/init.fs.sh
+++ ramdisk/init.fs.sh
@@ -15,5 +15,9 @@ mount -t f2fs \
           exit 0 )
 
 # EXT4
-sed -i -e 's@USERDATA@/dev/block/bootdevice/by-name/userdata    /data             ext4   nosuid,nodev,noatime,noauto_da_alloc             wait,check,fileencryption=ice,quota@g' /fstab.qcom
+if grep -q fileencryption=ice /vendor/etc/fstab.qcom; then
+  sed -i -e 's@USERDATA@/dev/block/bootdevice/by-name/userdata    /data             ext4   nosuid,nodev,noatime,noauto_da_alloc             wait,check,fileencryption=ice,quota@g' /fstab.qcom
+else
+  sed -i -e 's@USERDATA@/dev/block/bootdevice/by-name/userdata    /data             ext4   nosuid,nodev,noatime,noauto_da_alloc             wait,check,quota@g' /fstab.qcom
+fi
 touch fstab.ready
