1b3e7e551a5240592f4cef48025921573f6a3d77
Park Ju Hyung <qkrwngud825@gmail.com>
2018-01-04 23:29:48 +0900

    build_kernel: use LZ4
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -47,13 +47,13 @@ cd $KERNELDIR
 rm -rf $RAMFS_TMP/tmp/*
 
 cd $RAMFS_TMP
-find . | fakeroot cpio -H newc -o | gzip -9 > $RAMFS_TMP.cpio.gz
-ls -lh $RAMFS_TMP.cpio.gz
+find . | fakeroot cpio -H newc -o | lz4 -l -9 > $RAMFS_TMP.cpio.lz4
+ls -lh $RAMFS_TMP.cpio.lz4
 cd $KERNELDIR
 
 echo "Making new boot image"
 gcc -w -s -pipe -O2 -Itools/libmincrypt -o tools/mkbootimg/mkbootimg tools/libmincrypt/*.c tools/mkbootimg/mkbootimg.c
-tools/mkbootimg/mkbootimg --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb --ramdisk $RAMFS_TMP.cpio.gz --cmdline 'androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 buildvariant=user' --base 0x00008000 --pagesize 4096 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o $KERNELDIR/boot.img
+tools/mkbootimg/mkbootimg --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb --ramdisk $RAMFS_TMP.cpio.lz4 --cmdline 'androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 sched_enable_hmp=1 sched_enable_power_aware=1 service_locator.enable=1 swiotlb=2048 buildvariant=user' --base 0x00008000 --pagesize 4096 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second_offset 0x00f00000 -o $KERNELDIR/boot.img
 GENERATED_SIZE=$(stat -c %s boot.img)
 if [[ $GENERATED_SIZE -gt $PARTITION_SIZE ]]; then
 	echo "boot.img size larger than partition size!" 1>&2
