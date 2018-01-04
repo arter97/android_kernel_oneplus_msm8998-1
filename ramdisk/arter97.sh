790855d57d253bd4dd9331f4f6e4ba59cee498ec
Park Ju Hyung <qkrwngud825@gmail.com>
2018-01-05 00:11:13 +0900

    ramdisk: merge tunables from Paranoid Android
    Credits to TheCrazyLex
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/arter97.sh
+++ ramdisk/arter97.sh
@@ -19,5 +19,22 @@ echo "0:1248000 4:1344000" > /sys/module/cpu_boost/parameters/input_boost_freq
 echo 90 > /sys/module/cpu_boost/parameters/input_boost_ms
 echo "0:1171200 4:1190400" > /sys/module/cpu_boost/parameters/input_boost_freq_s2
 echo 150 > /sys/module/cpu_boost/parameters/input_boost_ms_s2
-chmod 444 /sys/module/cpu_boost/parameters/input_boost_ms
-chmod 444 /sys/module/cpu_boost/parameters/input_boost_freq_s2
+
+# Configure CPU governor
+echo 1 > /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction
+echo 1 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/enable_prediction
+echo 59000 > /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis
+
+# Disable console suspend
+echo N > /sys/module/printk/parameters/console_suspend
+
+# Disable userspace intervention
+chmod 444 \
+  /sys/module/cpu_boost/parameters/input_boost_freq \
+  /sys/module/cpu_boost/parameters/input_boost_ms \
+  /sys/module/cpu_boost/parameters/input_boost_freq_s2 \
+  /sys/module/cpu_boost/parameters/input_boost_ms_s2 \
+  /sys/devices/system/cpu/cpu0/cpufreq/interactive/enable_prediction \
+  /sys/devices/system/cpu/cpu4/cpufreq/interactive/enable_prediction \
+  /sys/devices/system/cpu/cpu4/cpufreq/interactive/max_freq_hysteresis \
+  /sys/module/printk/parameters/console_suspend
