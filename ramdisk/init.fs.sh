54b58b90dc5045e02715feabe54c85ccb3d6da5e
Park Ju Hyung <qkrwngud825@gmail.com>
2018-02-12 05:38:12 +0900

    ramdisk: parenthesis forks a sub-shell
    After f2fs is detected, exit 0 is not being called directly after.
    This fixes custom libsqlite not being used with f2fs.
    
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- ramdisk/init.fs.sh
+++ ramdisk/init.fs.sh
@@ -11,8 +11,11 @@ mount -t f2fs \
         ( umount /data; \
           sed -i -e 's@USERDATA@/dev/block/bootdevice/by-name/userdata    /data             f2fs   nosuid,nodev,noatime,discard,background_gc=off   wait,check,formattable,quota@g' fstab.qcom; \
           touch fstab.ready; \
-          echo "Mounted userdata as f2fs"; \
-          exit 0 )
+          echo "Mounted userdata as f2fs"; )
+
+if [ -e fstab.ready ]; then
+  exit 0
+fi
 
 # EXT4
 if grep /dev/block/bootdevice/by-name/userdata /system/vendor/etc/fstab.qcom | grep -q fileencryption=ice; then
